82544d912fdaf971be1248e71113007f0b513ccb
joaopauloalbq <joaopauloalbu@gmail.com>
2020-01-11 18:59:37 -0300

    fix multiline selections and translineation

--- seltr.py
+++ seltr.py
@@ -16,7 +16,7 @@ def argparser():
 
 def translate(to_translate, to_language="pt", language="auto"):
     agents = {'User-Agent': "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)"}
-    link = "http://translate.google.com/m?hl=%s&sl=%s&q=%s" % (to_language, language, to_translate.replace(" ", "+"))
+    link = "http://translate.google.com/m?hl=%s&sl=%s&q=%s" % (to_language, language, to_translate.replace('-\n', '').replace('\n', '+').replace(' ', '+'))
     request = urllib.request.Request(url=link, headers=agents)
     page = urllib.request.urlopen(request).read().decode('utf-8')
     return page.split('class="t0">')[1].split('</div>')[0]
